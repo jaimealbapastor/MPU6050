e1bf4e13e6dcc50646b02a175384791cfe0c7700
Jaime Alba Pastor <jalbapastor@gmail.com>
2023-10-11 22:22:36 +0200

    ino

--- MPU-6050_Projet/MPU6050_Angle.cpp
+++ MPU-6050_Projet/MPU6050_Angle.cpp
@@ -2,23 +2,28 @@
 
 const double MPU6050_Angle::pi = 3.1415926535897932384626433832795;
 
-MPU6050_Angle::MPU6050_Angle() {
-  // Constructor
+MPU6050_Angle::MPU6050_Angle() {}
+
+MPU6050_Angle::~MPU6050_Angle()
+{
+  this->endSerial();
 }
 
-void MPU6050_Angle::begin() {
-  // Init module GY-512 
+void MPU6050_Angle::begin(int baud_rate = 9600)
+{
+  // Init module GY-512
   Wire.begin();
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(MPU_address);
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
 
   // Init serial port
-  Serial.begin(9600);
+  Serial.begin(baud_rate);
 }
 
-void MPU6050_Angle::readAngle() {
+void MPU6050_Angle::readAngle()
+{
   delay(100);
 
   // Read sensor data
@@ -28,57 +33,71 @@ void MPU6050_Angle::readAngle() {
   ComputeAngle(GyAccTemp, PitchRoll);
 
   // Display roll, pitch, and yaw angles in degrees
-  Serial.print(PitchRoll[0]); Serial.print(",");
-  Serial.print(PitchRoll[1]); Serial.print(",");
+  Serial.print(PitchRoll[0]);
+  Serial.print(",");
+  Serial.print(PitchRoll[1]);
+  Serial.print(",");
   Serial.println(PitchRoll[2]);
   delay(100);
 }
 
-void MPU6050_Angle::readRowGyro() {
+void MPU6050_Angle::readRowGyro()
+{
   delay(100);
 
   // Read sensor data
   ReadGY521(GyAccTemp, GATCorr);
 
   // Display Row output of gyroscope
-  Serial.print(GyAccTemp[4]); Serial.print(",");
-  Serial.print(GyAccTemp[5]); Serial.print(",");
+  Serial.print(GyAccTemp[4]);
+  Serial.print(",");
+  Serial.print(GyAccTemp[5]);
+  Serial.print(",");
   Serial.println(GyAccTemp[6]);
   delay(100);
 }
 
-void MPU6050_Angle::readRowAcc() {
+void MPU6050_Angle::readRowAcc()
+{
   delay(100);
 
   // Read sensor data
   ReadGY521(GyAccTemp, GATCorr);
 
   // Display Row outputs of accelermoter
-  Serial.print(GyAccTemp[0]); Serial.print(",");
-  Serial.print(GyAccTemp[1]); Serial.print(",");
+  Serial.print(GyAccTemp[0]);
+  Serial.print(",");
+  Serial.print(GyAccTemp[1]);
+  Serial.print(",");
   Serial.println(GyAccTemp[2]);
   delay(100);
 }
 
-void MPU6050_Angle::ReadGY521(int *GyAccTempp, int *GATCorrr) {
+void MPU6050_Angle::ReadGY521(int *GyAccTempp, int *GATCorrr)
+{
   // Sensor initialization
-  Wire.beginTransmission(MPU);
+  Wire.beginTransmission(MPU_address);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 14, true);
+  Wire.requestFrom(MPU_address, 14, true);
 
   // Read data (3 accelerometer axes + temperature + 3 gyroscope axes)
-  for (int i = 0; i < NumData; i++) {
-    if (i != 3) {
+  for (int i = 0; i < NumData; i++)
+  {
+    if (i != 3)
+    {
       GyAccTempp[i] = (Wire.read() << 8 | Wire.read()) + GATCorrr[i];
-    } else {
+    }
+    else
+    {
       GyAccTempp[i] = (Wire.read() << 8 | Wire.read()) + GATCorrr[i];
-      GyAccTempp[i] = GyAccTempp[i] / 340 + 36.53;        // We measure the temperature here and compute it to be in celsius 
+      GyAccTempp[i] = GyAccTempp[i] / 340 + 36.53; // We measure the temperature here and compute it to be in celsius
     }
   }
 }
 
-void MPU6050_Angle::ComputeAngle(int *GyAccTempp, double *PitchRol) {
+void MPU6050_Angle::ComputeAngle(int *GyAccTempp, double *PitchRol)
+{
   double x = GyAccTempp[0];
   double y = GyAccTempp[1];
   double z = GyAccTempp[2];
@@ -91,4 +110,10 @@ void MPU6050_Angle::ComputeAngle(int *GyAccTempp, double *PitchRol) {
   PitchRol[0] = PitchRol[0] * (180.0 / pi);
   PitchRol[1] = PitchRol[1] * (180.0 / pi);
   PitchRol[2] = PitchRol[2] * (180.0 / pi);
+}
+
+void MPU6050_Angle::endSerial()
+{
+  if (Serial)
+    Serial.end();
 }
--- MPU-6050_Projet/MPU6050_Angle.h
+++ MPU-6050_Projet/MPU6050_Angle.h
@@ -5,9 +5,11 @@
 #include <Wire.h>
 #include <math.h>
 
-class MPU6050_Angle {
+class MPU6050_Angle
+{
 public:
   MPU6050_Angle();
+  ~MPU6050_Angle();
   void begin();
   void readAngle();
   void readRowGyro();
@@ -15,11 +17,11 @@ public:
 
 private:
   static const int NumData = 7;
-  static const int MPU = 0x68; // Define MPU constant
-  static const double pi;      // Define pi constant
-  int GyAccTemp[NumData];      // Table with the raw values of the gyro and acc
-  int GATCorr[NumData] = {0, 0, 0, 0, 0, 0, 0};   // Table with a correction that can be added to the read values
-  double PitchRoll[3];         // Table with the angles
+  static const int MPU_address = 0x68;          // Define MPU constant
+  static const double pi;                       // Define pi constant
+  int GyAccTemp[NumData];                       // Table with the raw values of the gyro and acc
+  int GATCorr[NumData] = {0, 0, 0, 0, 0, 0, 0}; // Table with a correction that can be added to the read values
+  double PitchRoll[3];                          // Table with the angles
 
   void ReadGY521(int *GyAccTempp, int *GATCorrr);
   void ComputeAngle(int *GyAccTempp, double *PitchRol);
